543dc522fac9fc748f56db34de19722568ef6c51
NguyenCongTuanAnh <tuanem051203@gmail.com>
2023-09-05 16:32:37 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import Profile from './pages/Profile';
+import DefaultLayout from './component/GlobalStyles/layouts/DefaultLayout';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <DefaultLayout>
+                                <Home />
+                            </DefaultLayout>
+                        }
+                    ></Route>
+                    <Route
+                        path="/admin"
+                        element={
+                            <DefaultLayout>
+                                <Admin />
+                            </DefaultLayout>
+                        }
+                    ></Route>
+                    <Route path="/profile" element={<Profile />}></Route>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
