029fe6a6cb7a449ea51f4a39f816616863806b50
NguyenCongTuanAnh <tuanem051203@gmail.com>
2023-09-13 17:39:27 +0700

    edit Admin dashboard(Sidebar, Header, Footer, Content)

--- src/component/GlobalStyles/layouts/DefaultLayout/Header/index.js
+++ src/component/GlobalStyles/layouts/DefaultLayout/Header/index.js
@@ -1,5 +1,22 @@
 function Header() {
-    return <h1>Header</h1>;
+  return (
+    <header>
+      <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Trang chủ</a>
+          </li>
+          <li>
+            <a href="/about">Giới thiệu</a>
+          </li>
+          <li>
+            <a href="/contact">Liên hệ</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
